e869ad8e1826b82d09b3612da397b9ae0e3dc0a1
Beomsu Son <65557784+shon5544@users.noreply.github.com>
2023-11-13 05:24:46 +0900

    feat: login API 추가

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from crawler.crawler import Crawler
+from dto.loginRequestDto import LoginRequestDto
+from global_module.driverSetting import DriverSetting
 
 app = FastAPI()
+driverSetting = DriverSetting()
+crawler = Crawler(driverSetting)
 
 @app.get("/login")
-def login():
-    return "It's a Good Day"
+def login(loginRequestDto: LoginRequestDto):
+    userId = loginRequestDto.userId
+    userPw = loginRequestDto.userPw
+
+    crawler.login(userId, userPw)
+    crawler.quit()
