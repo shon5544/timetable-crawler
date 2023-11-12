bcaba04b603a43b32e8029932bbb54d9b31ee4ba
Beomsu Son <65557784+shon5544@users.noreply.github.com>
2023-11-13 02:56:08 +0900

    refactor: 안 쓰는 import 제거

--- global_module/driverSetting.py
+++ global_module/driverSetting.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 class DriverSetting:
     def __init__(self):
