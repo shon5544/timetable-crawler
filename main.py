1e88380528faf39c1198998eeed6448db4db4c6c
shon5544 <sbs8239@gmail.com>
2023-12-04 23:30:57 +0900

    fix: dto -> path variable 변경

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ app = FastAPI()
 
 
 # 테스트 용 api
-@app.get("/crawling")
-def crawling(crawlingRequestDto: CrawlingRequestDto):
+@app.get("/crawling/{userId}/{userPw}")
+def crawling(userId: str, userPw: str):
     driverSetting = DriverSetting()
     crawler = Crawler(driverSetting)
-
-    userId: str = crawlingRequestDto.userId
-    userPw: str = crawlingRequestDto.userPw
 
     crawler.login(userId, userPw)
     result = crawler.crawlingTimeTable()
